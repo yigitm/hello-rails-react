6fc73e0c232d908fafdb0a2103e923a7c84bf7ca
Yigit Mersin <yigitmersin@gmail.com>
2022-04-27 02:00:01 +0300

    fix package.json issue & re-set the react-rails

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -3,26 +3,12 @@ import PropTypes from 'prop-types';
 
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
-import HelloWorld from './HelloWorld';
-
-import configureStore from '../configureStore';
+import configureStore from '../redux/configureStore';
 const store = configureStore();
 
 class App extends React.Component {
   render() {
-    return (
-      <Provider store={store}>
-        <BrowserRouter>
-          <Switch>
-            <Route exact path="/" render={() => 'Home!'} />
-            <Route
-              path="/hello"
-              render={() => <HelloWorld greeting="Friend" />}
-            />
-          </Switch>
-        </BrowserRouter>
-      </Provider>
-    );
+    return <Provider store={store}></Provider>;
   }
 }
 
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,2 +1,9 @@
+console.log('Hello World from Webpacker');
 // Support component names relative to this directory:
 var componentRequireContext = require.context('components', true);
+//var ReactRailsUJS = require('react_ujs');
+//ReactRailsUJS.useContext(componentRequireContext);
+// Support component names relative to this directory:
+var componentRequireContext = require.context("components", true);
+var ReactRailsUJS = require("react_ujs");
+ReactRailsUJS.useContext(componentRequireContext);
