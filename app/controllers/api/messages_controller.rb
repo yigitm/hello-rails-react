38bd4aab944e91aa2f644727e0bcf45323ae7e3c
Yigit Mersin <yigitmersin@gmail.com>
2022-04-27 02:13:08 +0300

    Create api endpoint

--- app/controllers/api/messages_controller.rb
+++ app/controllers/api/messages_controller.rb
@@ -1,10 +1,11 @@
 
   class Api::MessagesController < ApplicationController
     def index
-      render json: { :messages => [
-        {
-          :name => 'some-stuff'
-        }
-      ] }.to_json
+      @message = Message.order('RANDOM()').first
+      if @message
+        render json: { success: true, message: 'Loaded greeting message', data: { greeting: @message } }, status: :ok
+      else
+        render json: { success: false, errors: 'Oops, Something went wrong!' }, status: :unprocessable_entity
+      end
     end
   end
