35d77ceed44a90c0cebbb02cf0e5c013450f7c12
Yigit Mersin <yigitmersin@gmail.com>
2022-04-27 03:44:24 +0300

    fix linter

--- app/controllers/api/messages_controller.rb
+++ app/controllers/api/messages_controller.rb
@@ -1,11 +1,10 @@
-
-  class Api::MessagesController < ApplicationController
-    def index
-      @message = Message.order('RANDOM()').first
-      if @message
-        render json: { success: true, message: 'Loaded greeting message', data: { greeting: @message } }, status: :ok
-      else
-        render json: { success: false, errors: 'Oops, Something went wrong!' }, status: :unprocessable_entity
-      end
+class Api::MessagesController < ApplicationController
+  def index
+    @message = Message.order('RANDOM()').first
+    if @message
+      render json: { success: true, message: 'Loaded greeting message', data: { greeting: @message } }, status: :ok
+    else
+      render json: { success: false, errors: 'Oops, Something went wrong!' }, status: :unprocessable_entity
     end
   end
+end
